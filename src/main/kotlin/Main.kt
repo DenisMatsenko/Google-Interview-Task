c9a6d72160f74b99eafa35ce377607ee08849b26
hard.tender.blade <92087058+DenisMatsenko@users.noreply.github.com>
2023-03-07 11:20:59 +0100

    Code refactoring

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,18 @@
 import com.github.mm.coloredconsole.colored
 
 fun main() {
-
     while (true) {
+        //Thanks Marcel Matula for such a handy tool!
         colored {
             print("Enter the code: ".bold)
             val code: String = readLine().toString()
 
             println("Decoding: ${code.green.bold} -> ${Decode(code, 1).yellow.bold}")
-
         }
     }
 }
 
+//Recursive method that calls itself if it finds a number in the string
 fun Decode(code: String, factor: Int): String {
     var decodedStr: String = ""
 
@@ -20,29 +20,37 @@ fun Decode(code: String, factor: Int): String {
     while (i < code.length) {
         var char: Char = code[i]
 
+        //Adding letters
         if (char.isLetter())
             decodedStr += char
 
+        //Run this method if it finds a number
         if (code[i].isDigit()) {
             var start = i + 1
             var end = FindEndIndex(code, start)
 
             decodedStr += Decode(code.substring(start, end + 1), char.digitToInt())
-            i = end
+            i = end //Skips brackets
         }
         i++
     }
 
+    return Multiplication(decodedStr, factor)
+}
+
+//Multiplying the decoded string in brackets by its multiplier
+fun Multiplication(decodedStr: String, factor: Int): String {
     var result: String = ""
-    var u: Int = 0
-    while (u < factor) {
+    var i: Int = 0
+    while (i < factor) {
         result += decodedStr
-        u++
+        i++
     }
 
     return result
 }
 
+//Method returns the index of the closing brace
 fun FindEndIndex(code: String, start: Int): Int {
     var skip: Int = 1
     var i: Int = start
